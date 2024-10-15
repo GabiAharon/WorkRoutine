229f16d2877f219f7819c01f9b05a061f0a1120d
GabiAharon <146546637+GabiAharon@users.noreply.github.com>
2024-10-15 13:40:10 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,11 @@
-const CACHE_NAME = 'my-work-routines-v1';
+const CACHE_NAME = 'work-routine-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/styles.css',
-  '/manifest.json',
+  '/script.js',
   '/icon-192x192.png',
-  '/icon-512x512.png',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Assistant:wght@400;700&display=swap',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js',
-  'https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js'
+  '/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -23,41 +18,6 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(
-          function(response) {
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then(response => response || fetch(event.request))
   );
 });
